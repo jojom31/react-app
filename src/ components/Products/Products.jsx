712e0617bd9686b427b54401b368ba9d08ce8878
JoJo Myles <joneshiamyles31@gmail.com>
2021-06-08 10:23:39 -0400

    started working on the eccommerce site

---
+++ src/ components/Products/Products.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product";
+
+const products = [
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running Shoes.",
+    price: "$80",
+    image:
+      "https://i0.wp.com/post.healthline.com/wp-content/uploads/2020/01/268453-Best_Running_Shoes_for_Flat_Feet-1296x728-header-1296x728.jpg?w=1155&h=1528",
+  },
+  {
+    id: 2,
+    name: "Macbook",
+    description: "Apple Macbook.",
+    price: "$315",
+    image:
+      "https://i.pcmag.com/imagery/reviews/05CbcW9cP4o0rqbCnVB2OFZ-1..1584707541.jpg",
+  },
+];
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
